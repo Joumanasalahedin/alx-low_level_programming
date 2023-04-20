dd05362c15fec3e92ede8a36dc305b9973abc916
Joumana Arafa <joumanasalahedin@gmail.com>
2023-04-21 00:51:23 +0200

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
   * Return: Alwats 0 (Success)
   */
 int main(void)
-{;
-	puts("\"Programming is like building a multilingual puzzle\n");
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
