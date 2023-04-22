ed50699987fae35490ad35c3c8fd87593c11229e
Joumana Arafa <joumanasalahedin@gmail.com>
2023-04-22 02:12:30 +0200

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,8 +13,11 @@ int main(void)
 	for (num = 48; num < 58; num++)
 	{
 		putchar(num);
-		putchar(',');
-		putchar(' ');
+		while (num != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 
 	putchar('\n');
