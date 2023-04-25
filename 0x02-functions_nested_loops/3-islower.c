d6eaaf6349eafc8ee6d7f253221aa2db785743aa
Joumana Arafa <joumanasalahedin@gmail.com>
2023-04-26 01:06:58 +0200

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * _ilower - checks for lowercase character
+  *
+  * Return: int c
+  */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return 1;
+	}
+	
+	else
+	{
+		return 0
+	}
+}
