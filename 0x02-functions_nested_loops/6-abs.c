f1b780de69cf2f68651b9591c6d8adf91643801b
Joumana Arafa <joumanasalahedin@gmail.com>
2023-04-26 01:43:22 +0200

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  * _abs - computes the absolute value of an integer
+  *
+  * @int: integar
+  *
+  * Return: absolute value
+  */
+
+int _abs(int)
+{
+	if (int < 0)
+	{
+		int = 0 - int;
+		_putchar(int);
+	}
+
+	else if (int > 0)
+	{
+		_putchar(int);
+	}
+
+	else if (int == 0)
+	{
+		_putchar('0');
+	}
+	return (0);
+}
