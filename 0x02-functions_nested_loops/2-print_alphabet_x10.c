bef920574ef48062e0a19451aa19545d90826fc4
Joumana Arafa <joumanasalahedin@gmail.com>
2023-04-26 00:55:23 +0200

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * print_alphabet_x10 - print the alphabet 10x in lowercase and a new line
+  *
+  * Return: Always 0
+  */
+
+void print_alphabet_x10(void)
+{
+	char alphabet;
+	int counter;
+
+	for (counter = 0; counter < 10; counter++)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+		}
+	}
+	putchar('\n');
+}
