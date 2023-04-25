3563c038e55d77f4c267d651b3b217057e072e0f
Joumana Arafa <joumanasalahedin@gmail.com>
2023-04-25 23:15:54 +0200

    Created files and Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print _putchar and a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char str[]: "_putchar\n";
+	int counter;
+
+	for (counter = 0; str[counter] != '\0'; counter++)
+	{	
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
