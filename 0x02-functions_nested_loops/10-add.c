5b66297e47ce4039c18c63877322c7dd0b30eb2f
Joumana Arafa <joumanasalahedin@gmail.com>
2023-04-26 03:01:44 +0200

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * add - adds 2 integers
+  *
+  * @a: frst integer
+  * @b: second integer
+  *
+  * Return: result
+  */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
