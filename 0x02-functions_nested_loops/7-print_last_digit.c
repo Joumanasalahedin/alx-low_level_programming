e7214f522de28f4d90a8ae8e47a398ece93d826d
Joumana Arafa <joumanasalahedin@gmail.com>
2023-04-26 02:02:31 +0200

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+include "main.h"
+
+/**
+  * print_last_digit - prints the last digit of a number
+  *
+  * @n: input of a number
+  *
+  * Return: value of last digit
+  */
+
+int print_last_digit(int n);
+{
+	int last;
+
+	last = n % 10;
+
+	if (last < 0)
+	{
+		return (-last);
+	}
+
+	else if (last > 0)
+	{
+		return (last);
+	}
+}
