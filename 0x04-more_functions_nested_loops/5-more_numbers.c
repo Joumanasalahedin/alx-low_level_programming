d1ca445c1adcea1b4e23f3667e7b0b720a711894
Joumana Arafa <joumanasalahedin@gmail.com>
2023-04-27 16:39:59 +0200

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * more_numbers - prints numbers 0 to 14
+  *
+  * Return: numbers 0 to 14 10 times
+  */
+
+void more_numbers(void)
+{
+	int a, count;
+
+	for (count = 0; count < 10; count++)
+	{
+		for (a = 0; a <= 14; a++)
+		{
+			if (a >= 10)
+			{
+				_putchar((a / 10) + 48);
+			}
+			_putchar((a % 10) + 48);
+		}
+	}
+	_putchar('\n');
+}
