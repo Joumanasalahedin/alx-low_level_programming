f24c269b14a28424eac91c355b5b07b9c17a4880
Joumana Arafa <joumanasalahedin@gmail.com>
2023-04-27 22:42:53 +0200

    Task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+  * print_triangle - prints a triangle
+  *
+  * @size: size of the triangle
+  *
+  * Return: triangle based on size given
+  */
+
+void print_triangle(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+	else if (size > 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 1; b < size - a; b++)
+			{
+				_putchar(' ');
+			}
+
+			for (b--; b < size; b++)
+			{
+				_putchar('#');
+			}
+			
+			if (a < (size - 1))
+			{
+				_putchar('\n');
+			}
+		}
+	}
+	_putchar('\n');
+}
