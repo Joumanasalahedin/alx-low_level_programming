ed4a6bca31f1cedc9769b757a4e703821e361c76
Joumana Arafa <joumanasalahedin@gmail.com>
2023-04-27 17:41:57 +0200

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * print_line - draws a straight line in the terminal
+  *
+  * @n: number of times _ should be printed
+  *
+  * Return - draws a straight line
+  */
+
+void print_line(int n)
+{
+	int a;
+
+	if (n > 0)
+	{
+		for (a = 0; a <= n, a++)
+		{
+			_putchar(95);
+		}
+	}
+
+	else if (n <= 0)
+		break;
+
+	_putchar('\n');
+}
