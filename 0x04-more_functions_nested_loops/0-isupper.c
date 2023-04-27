6e0d44e46dd54808c8f03ad053939d148452d780
Joumana Arafa <joumanasalahedin@gmail.com>
2023-04-27 15:37:37 +0200

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * _isupper - Checks for uppercase characters
+  *
+  * @c: character
+  *
+  * Return: 1 if uppercase, 0 otherwise
+  */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
