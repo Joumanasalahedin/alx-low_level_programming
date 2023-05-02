6079c6458d3de99392d9fa8758b0c8c9dd542629
Joumana Arafa <joumanasalahedin@gmail.com>
2023-05-02 20:59:04 +0200

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+  * rev_string - reverses a string
+  *
+  * @s: string
+  *
+  * Return: reversed string
+  */
+
+void rev_string(char *s)
+{
+	char temp;
+	char *start = s;
+	char *end =s;
+
+	while (*end)
+	{
+		end++;
+	}
+
+	end--;
+
+	while (start < end)
+	{
+		temp = *start;
+		*start = *end;
+		*end = temp;
+		start++;
+		end--;
+	}
+
+	_putchar('\n');
+}
