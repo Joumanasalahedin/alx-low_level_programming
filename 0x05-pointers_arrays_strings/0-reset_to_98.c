9c25cae40f4c6d563fb74dd7047c61d8e5197585
Joumana Arafa <joumanasalahedin@gmail.com>
2023-05-02 19:01:01 +0200

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - updates value and points to 98
+  *
+  * @*n: pointer to n
+  *
+  * Return: updates to 98
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
