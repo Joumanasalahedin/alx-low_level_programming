e7646854ea64f3ab3f7f19b9082e740a95b1281f
Joumana Arafa <joumanasalahedin@gmail.com>
2023-05-02 20:25:06 +0200

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * print_rev - prints a string in reverse
+  *
+  * @s: string
+  *
+  * Return: prints string in reverse then a new line
+  */
+
+void print_rev(char *s)
+{
+	for (*str = '\0'; ; str--)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
