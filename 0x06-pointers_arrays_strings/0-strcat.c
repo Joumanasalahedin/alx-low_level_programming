83fa13402749b9816d1713a0ee4bbaf0f9c8e976
Joumana Arafa <joumanasalahedin@gmail.com>
2023-05-04 16:02:29 +0200

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+  * _strcat - concatenates two strings
+  * @dest: first string
+  * @src: second string
+  * Return: dest
+  */
+
+char *_strcat(char *dest, char *src)
+{
+	int a = 0;
+	int b = 0;
+
+	char *str1;
+	char *str2;
+
+	str1 = dest;
+	str2 = src;
+
+	while(dest[a] != '\0')
+	{
+		++str1;
+		a++;
+	}
+
+	while(src[b] != '\0')
+	{
+		*str1 = *str2;
+		str1++;
+		str2++;
+		b++;
+	}
+
+	return (dest);
+}
