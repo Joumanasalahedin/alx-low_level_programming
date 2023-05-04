cfb7a6cd9fa93ad22c02901545d8d8890439ab63
Joumana Arafa <joumanasalahedin@gmail.com>
2023-05-04 16:03:14 +0200

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -18,13 +18,13 @@ char *_strcat(char *dest, char *src)
 	str1 = dest;
 	str2 = src;
 
-	while(dest[a] != '\0')
+	while (dest[a] != '\0')
 	{
 		++str1;
 		a++;
 	}
 
-	while(src[b] != '\0')
+	while (src[b] != '\0')
 	{
 		*str1 = *str2;
 		str1++;
