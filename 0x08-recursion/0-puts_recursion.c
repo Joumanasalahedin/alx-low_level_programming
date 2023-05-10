aeda6f4f125d9ba53a437254e5c9607689ffd84d
Joumana Arafa <joumanasalahedin@gmail.com>
2023-05-10 18:03:33 +0200

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,14 +6,14 @@
   * Return: no return
   */
 
-void _puts_recusrion(char *s)
+void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-	
+
 	else
 	{
 		_putchar('\n');
