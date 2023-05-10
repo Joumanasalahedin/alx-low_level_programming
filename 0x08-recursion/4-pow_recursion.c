8967e716643121b9b541e5f483a5ab120906af27
Joumana Arafa <joumanasalahedin@gmail.com>
2023-05-10 19:34:04 +0200

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "manin.h"
+
+/**
+  * _pow_recursion - value of x rasied to the power of y
+  * @x: base
+  * @y: exponent
+  * Return: x to the power of y
+  */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
