6a059239813328d87c768c46b6a824e491b2066e
Joumana Arafa <joumanasalahedin@gmail.com>
2023-05-12 21:00:51 +0200

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - multiplies two numbers
+  * @argc: number of command line arguments
+  * @argv: array that contains the program command line arguments
+  * Return: 0 (Success)
+  */
+
+int main(int argc, char *argv[])
+{
+	int product = 1;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	product *= atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", product);
+	return (0);
+}
