459e32b34b391050efcc1a05c17a96f255732e1f
Joumana Arafa <joumanasalahedin@gmail.com>
2023-05-12 21:29:20 +0200

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,23 +13,17 @@ int main(int argc, char *argv[])
 {
 	int i, j, sum = 0;
 
-	if (argc == 1)
-		printf("0\n");
-
-	else
+	for (i = 1; i < argc; i++)
 	{
-		for (i = 1; i < argc; i++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			for (j = 0; argv[i][j] != '\0'; j++)
+			if (!isdigit(argv[i][j]))
 			{
-				if (!isdigit(argv[i][j]))
-				{
-					printf("Error\n");
-					return (1);
-				}
+				printf("Error\n");
+				return (1);
 			}
-			sum += atoi(argv[i]);
 		}
+		sum += atoi(argv[i]);
 	}
 
 	printf("%d\n", sum);
