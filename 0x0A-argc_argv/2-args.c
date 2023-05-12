cfad46662f2dbbf8fcbfb5a54e6b33a05b056adc
Joumana Arafa <joumanasalahedin@gmail.com>
2023-05-12 20:47:08 +0200

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - prints all arguments it receieves
+  * argc: number of comand line arguments
+  * argv: array that contains the program command line arguments
+  * Return: 0 (Success)
+  */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
