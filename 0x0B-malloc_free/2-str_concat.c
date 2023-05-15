731458b7b079c1945f24e9927df44310c903cd8d
Joumana Arafa <joumanasalahedin@gmail.com>
2023-05-15 22:08:40 +0200

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,6 +14,12 @@ char *str_concat(char *s1, char *s2)
 	unsigned int len = 0, l = 0;
 	unsigned int i, j;
 
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
 	while (s1[len] != '\0')
 		len++;
 
