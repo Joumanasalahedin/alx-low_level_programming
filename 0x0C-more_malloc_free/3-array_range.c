0572e5ee7972939a23c98a80e9d2c8544bf17492
Joumana Arafa <joumanasalahedin@gmail.com>
2023-05-18 22:56:40 +0200

    task 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,7 +16,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	arr = malloc((max - min + 1) * sizeof(int));
+	arr = malloc((max - min + 1) * sizeof(*arr));
 
 	if (arr == NULL)
 		return (NULL);
