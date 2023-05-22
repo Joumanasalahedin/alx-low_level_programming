f9554f56c13a100def9fdde586a3ff6dcc114e79
Joumana Arafa <joumanasalahedin@gmail.com>
2023-05-23 00:58:01 +0200

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+  * init_dog - initializes a variable of type struct dog
+  * @d: pointer to a variable of type struct dog
+  * @name: name of dog
+  * @age: dog's age
+  * @owner: dog's owner
+  * Return: void
+  */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == 0)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner
+}
