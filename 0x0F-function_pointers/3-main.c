dee22d4bbb2cd754dc20b3445f19424df6cb11ee
Joumana Arafa <joumanasalahedin@gmail.com>
2023-05-25 00:09:40 +0200

    task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -26,12 +26,6 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	if (b == 0 && (*argv[2] == '/' || *argv[2] == '%'))
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
 	result = get_op_func(argv[2])(a, b);
 	printf("%d\n", result);
 	return (0);
