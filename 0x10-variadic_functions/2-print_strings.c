3e6f074880ff174d8b81452c68c362f5e32a07b4
Joumana Arafa <joumanasalahedin@gmail.com>
2023-05-26 00:00:57 +0200

    task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			else
 				printf("%s", va_arg(list, char *));
 
-			if (i < n - 1)
+			if (i < n - 1 && separator)
 				printf("%s", separator);
 		}
 		printf("\n");
