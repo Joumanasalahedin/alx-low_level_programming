a42e63c549cbab09e695ba343a32a9b1b9a93642
Joumana Arafa <joumanasalahedin@gmail.com>
2023-05-26 00:31:21 +0200

    task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,51 @@
+#include "variadic_functions.h"
+
+/**
+  * print_all - prints anything
+  * @format: list of types of arguments passed
+  * Return: void
+  */
+
+void print_all(const char * const format, ...)
+{
+	va_list list;
+	unsigned int i = 0, j, a = 0;
+	char *str;
+	const char types[] = "cifs";
+
+	va_start(list, format);
+	while (format != NULL && format[i] != '\0')
+	{
+		j = 0;
+		while (types[j])
+		{
+			if (format[i] == types[j] && c == 1)
+			{
+				printf(", ");
+				break;
+			} j++;
+		}
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(list, int)), c = 1;
+				break;
+			case 'i':
+				printf("%d", va_arg(list, int)), c = 1;
+				break;
+			case 'f':
+				printf("%f", va_arg(list, double)), c = 1;
+				break;
+			case 's':
+				str = va_arg(list, char *), c = 1;
+				if (str == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", str);
+				break;
+		} i++;
+	}
+	printf("\n"), va_end(list);
+}
