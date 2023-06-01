01fd2c3e63f4af2192ae194f5e688613662d7868
Joumana Arafa <joumanasalahedin@gmail.com>
2023-06-01 18:11:17 +0200

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <lists.h>
+
+/**
+  * bmain - function exexcuted before main
+  * Return: void
+  */
+
+void __attribute ((constructor)) bmain()
+{
+	printf("You're best! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
