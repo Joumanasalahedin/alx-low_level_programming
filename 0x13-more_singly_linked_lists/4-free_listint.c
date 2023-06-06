849fcaa2b0f6428edb185c3daafd6add2c75c20d
Joumana Arafa <joumanasalahedin@gmail.com>
2023-06-07 00:04:06 +0200

    task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+  * free_listint - frees a listint_t list
+  * @head: pointer to head of list
+  */
+
+void free_listint(listint_t *head)
+{
+	listint_t *current = head;
+	listint_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current->n);
+		free(current);
+		current = next;
+	}
+}
